e347e061f5f178ca0e3387881229e90e82215a2c
Drizzy122 <C21364936@mytudublin.ie>
2023-01-18 18:24:24 +0000

    changed

--- vrgame/Assets/ActivateGrabRay.cs
+++ vrgame/Assets/ActivateGrabRay.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ActivateGrabRay : MonoBehaviour
 {
+    public GameObject leftGrabRay;
+    public GameObject rightGrabRay;
+
+    public XRDirectInteractor leftDirectGrab;
+    public XRDirectInteractor rightDirectGrab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,7 @@ public class ActivateGrabRay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        leftGrabRay.SetActive(leftDirectGrab.interactablesSelected.Count == 0);
+        rightGrabRay.SetActive(rightDirectGrab.interactablesSelected.Count == 0);
     }
 }
